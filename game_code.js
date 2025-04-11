2ae41cbf4165b89e1e6c58ccdb5b8a7ccfc4086e
hemant-2023 <sv734615@gmail.com>
2025-04-11 17:31:22 +0530

    Initial commit

---
+++ game_code.js
@@ -0,0 +1,137 @@
+class RocketGame extends Phaser.Scene {
+    constructor() {
+        super({ key: 'RocketGame' });
+        this.survivalTime = 60;
+        this.rockSpeed = 200;
+        this.gameIsOver = false;
+    }
+
+    preload() {
+        this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('bullet', 'assets/bullet.png');
+        this.load.image('rock', 'assets/rock.png');
+    }
+
+    create() {
+        this.targetScore = Phaser.Math.Between(500, 800);
+
+        this.rocket = this.physics.add.image(400, 550, 'rocket').setCollideWorldBounds(true);
+
+        this.bullets = this.physics.add.group();
+        this.lastFired = 0;
+
+        this.rocks = this.physics.add.group();
+        this.spawnRock();
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '24px', fill: '#fff' });
+        this.timerText = this.add.text(16, 50, 'Time: 60', { fontSize: '24px', fill: '#fff' });
+        this.targetScoreText = this.add.text(600, 16, 'Target: ' + this.targetScore, { fontSize: '24px', fill: '#fff' });
+
+        this.physics.add.overlap(this.bullets, this.rocks, this.destroyRock, null, this);
+        this.physics.add.overlap(this.rocket, this.rocks, this.gameOver, null, this);
+
+        this.timerEvent = this.time.addEvent({
+            delay: 1000,
+            callback: () => {
+                this.survivalTime--;
+
+                if (this.survivalTime % 10 === 0) {
+                    this.rockSpeed = Math.min(this.rockSpeed + 50, 600);
+                }
+
+                this.timerText.setText('Time: ' + this.survivalTime);
+
+                if (this.survivalTime <= 0 && this.score < this.targetScore) {
+                    this.endGame('You Lose!');
+                }
+            },
+            callbackScope: this,
+            loop: true
+        });
+    }
+
+    update(time) {
+        if (this.gameIsOver) return;
+
+        if (this.cursors.left.isDown) {
+            this.rocket.setVelocityX(-300);
+        } else if (this.cursors.right.isDown) {
+            this.rocket.setVelocityX(300);
+        } else {
+            this.rocket.setVelocityX(0);
+        }
+
+        if (this.spaceKey.isDown && time > this.lastFired) {
+            const bullet = this.bullets.create(this.rocket.x, this.rocket.y - 20, 'bullet');
+            bullet.setVelocityY(-400);
+            this.lastFired = time + 300;
+        }
+
+        if (time % 1000 < 20) {
+            this.spawnRock();
+        }
+    }
+
+    spawnRock() {
+        if (this.gameIsOver) return;
+        const x = Phaser.Math.Between(50, 750);
+        const rock = this.rocks.create(x, 0, 'rock');
+        rock.setVelocityY(this.rockSpeed);
+    }
+
+    destroyRock(bullet, rock) {
+        bullet.destroy();
+        rock.destroy();
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+
+        if (this.score >= this.targetScore) {
+            this.endGame('You Win!');
+        }
+    }
+
+    gameOver(rocket, rock) {
+        this.endGame('Game Over');
+    }
+
+    endGame(message) {
+        this.physics.pause();
+        this.rocket.setTint(0xff0000);
+        this.add.text(250, 300, message, { fontSize: '48px', fill: '#fff' });
+        this.timerEvent.remove();
+        this.gameIsOver = true;
+
+        // Restart Button
+        const restartButton = this.add.text(330, 380, 'Restart', {
+            fontSize: '32px',
+            fill: '#0f0',
+            backgroundColor: '#222',
+            padding: { x: 20, y: 10 },
+            borderRadius: 10
+        }).setInteractive();
+
+        restartButton.on('pointerdown', () => {
+            this.scene.restart();
+        });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: RocketGame
+};
+
+const game = new Phaser.Game(config);
